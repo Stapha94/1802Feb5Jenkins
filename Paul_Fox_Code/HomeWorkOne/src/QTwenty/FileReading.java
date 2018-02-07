a5d137954f6680ca89ec67084d2a3209200feaf4
Paul Fox <pfoxiii@gmail.com>
2018-02-07 18:32:27 -0500

    Finished Question 20

---
+++ Paul_Fox_Code/HomeWorkOne/src/QTwenty/FileReading.java
@@ -0,0 +1,48 @@
+package QTwenty;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class FileReading {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader myBReader = null;
+
+        try {
+
+            myBReader = new BufferedReader(new FileReader("resources/Data.txt"));
+            String line;
+
+            while((line = myBReader.readLine()) != null){
+
+                String[] data = line.split(":");
+                System.out.println("Name: " + data[0] + " " + data[1] + "\nAge " + data[2] + " Years\nState: " + data[3] + " State");
+
+            }
+
+        } catch (FileNotFoundException e){
+
+            e.printStackTrace();
+
+        } catch (IOException e){
+
+            e.printStackTrace();
+
+        } finally {
+
+            if (myBReader != null){
+
+                myBReader.close();
+
+            }
+
+        }
+
+
+    }
+
+}
