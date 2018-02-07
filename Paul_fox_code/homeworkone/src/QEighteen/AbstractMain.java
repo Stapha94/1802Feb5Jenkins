9d17817e0f1396eab472460cd2ea0629449e5db1
Paul Fox <pfoxiii@gmail.com>
2018-02-07 15:22:51 -0500

    Added class that extends AbstractClass and has 'empty' methods for it

--- Paul_fox_code/homeworkone/src/QEighteen/AbstractMain.java
+++ Paul_fox_code/homeworkone/src/QEighteen/AbstractMain.java
@@ -1,11 +1,32 @@
 package QEighteen;
 
-public class AbstractMain {
+public class AbstractMain extends AbstractClass{
 
     public static void main(String[] args) {
 
-        
 
+
+    }
+
+    @Override
+    public boolean checkUppercase(String s){
+
+
+        return false;
+    }
+
+    @Override
+    public String lowerToUpper(String s){
+
+
+        return s;
+    }
+
+    @Override
+    public int stringToInt(String s){
+
+
+        return 0;
     }
 
 }
