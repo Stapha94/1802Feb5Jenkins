b817c534edea845783dec8e7db363f720d033f7b
Paul Fox <pfoxiii@gmail.com>
2018-02-07 18:17:27 -0500

    Finished Question 19

--- Paul_Fox_Code/HomeWorkOne/src/QNineteen/IntegerArrayList.java
+++ Paul_Fox_Code/HomeWorkOne/src/QNineteen/IntegerArrayList.java
@@ -37,6 +37,39 @@ public class IntegerArrayList {
         System.out.println("The sum of all evens is: " + evenSum);
         System.out.println("The sum of all odds is: " + oddSum);
 
+        for (int i = intList.size() - 1; i >=0; i--){
+
+            if(isPrime(intList.get(i))){
+
+                intList.remove(i);
+
+            }
+
+        }
+
+        for (int i = 0; i < intList.size(); i++){
+
+            System.out.println(intList.get(i));
+
+        }
+
+    }
+
+    public static boolean isPrime(int n) {
+
+        for(int i = 2 ;i < n; i++) {
+
+            if(n % i == 0)
+
+                return false;
+
+        }
+        if(n == 2){
+
+            return false;
+
+        }
+        return true;
 
     }
 
