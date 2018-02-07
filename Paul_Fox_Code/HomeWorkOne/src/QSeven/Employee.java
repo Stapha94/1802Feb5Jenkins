01c3df77a0c20174bbd90f9d79c3a4b7e69a7570
Paul Fox <pfoxiii@gmail.com>
2018-02-06 19:40:39 -0500

    Question 7 Finished

--- Paul_Fox_Code/HomeWorkOne/src/QSeven/Employee.java
+++ Paul_Fox_Code/HomeWorkOne/src/QSeven/Employee.java
@@ -1,10 +1,15 @@
 package QSeven;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Employee {
 
-    String name;
-    String dept;
-    int age;
+    public String name;
+    public String dept;
+    public int age;
+    public static ArrayList<Employee> employeeList = new ArrayList<>();
 
     public Employee(String name, String dept, int age){
 
@@ -14,10 +19,78 @@ public class Employee {
 
     }
 
+    public String toString(){
+
+        return this.name + " " + this.dept + " " + this.age;
+
+    }
+
     public static void main(String[] args) {
 
         Employee employeeOne = new Employee("Bob", "Marketing", 27);
         Employee employeeTwo = new Employee("Seth", "IT", 24);
+        employeeList.add(employeeOne);
+        employeeList.add(employeeTwo);
+
+        System.out.println("Unsorted");
+        for(int i = 0; i < employeeList.size(); i++){
+
+            System.out.println(employeeList.get(i));
+
+        }
+
+        System.out.println("\nSorted by Name");
+        for(int i = 0; i < employeeList.size(); i++){
+
+            Collections.sort(employeeList, new SortByName());
+            System.out.println(employeeList.get(i));
+
+        }
+
+        System.out.println("\nSorted by Dept");
+        for(int i = 0; i < employeeList.size(); i++){
+
+            Collections.sort(employeeList, new SortByDept());
+            System.out.println(employeeList.get(i));
+
+        }
+
+        System.out.println("\nSorted by Age");
+        for(int i = 0; i < employeeList.size(); i++){
+
+            Collections.sort(employeeList, new SortByAge());
+            System.out.println(employeeList.get(i));
+
+        }
+    }
+
+}
+
+class SortByName implements Comparator<Employee>{
+
+    public int compare(Employee a, Employee b){
+
+        return a.name.compareTo(b.name);
+
+    }
+
+}
+
+class SortByDept implements Comparator<Employee>{
+
+    public int compare(Employee a, Employee b){
+
+        return a.dept.compareTo(b.dept);
+
+    }
+
+}
+
+class SortByAge implements Comparator<Employee>{
+
+    public int compare(Employee a, Employee b){
+
+        return a.age - b.age;
 
     }
 
