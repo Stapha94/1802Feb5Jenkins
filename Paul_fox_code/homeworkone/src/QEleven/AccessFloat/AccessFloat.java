0eb8674f072361852870f9eca351b45ec9ccd83d
Paul Fox <pfoxiii@gmail.com>
2018-02-07 09:18:30 -0500

    Finished Question 11

---
+++ Paul_fox_code/homeworkone/src/QEleven/AccessFloat/AccessFloat.java
@@ -0,0 +1,15 @@
+package QEleven.AccessFloat;
+
+import QEleven.FloatStorage.*;
+
+public class AccessFloat {
+
+    public static void main(String[] args) {
+
+        System.out.println(Floats.x);
+        System.out.println(Floats.y);
+        System.out.println(Floats.x + Floats.y);
+
+    }
+
+}
---
+++ Paul_fox_code/homeworkone/src/QEleven/FloatStorage/Floats.java
@@ -0,0 +1,8 @@
+package QEleven.FloatStorage;
+
+public class Floats {
+
+    public static float x = (float)1.22;
+    public static float y = (float)3.10;
+
+}
