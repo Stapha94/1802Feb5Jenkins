094fcd213f0d9b15f9f5aa56efc7aad42b7a9ce6
Paul Fox <pfoxiii@gmail.com>
2018-02-26 10:02:49 -0500

    Added functionality for the result service.  All services now have basic functionality

--- Paul_Fox_Code/TREE/src/main/java/com/revature/services/ResultService.java
+++ Paul_Fox_Code/TREE/src/main/java/com/revature/services/ResultService.java
@@ -1,4 +1,33 @@
 package com.revature.services;
 
+import com.revature.beans.Result;
+import com.revature.dao.ResultDaoJdbcPg;
+
 public class ResultService {
+
+    private static ResultDaoJdbcPg myResultDao = new ResultDaoJdbcPg();
+
+    public Result getById(int resultId){
+
+        return myResultDao.getById(resultId);
+
+    }
+
+    public void newResult(Result result){
+
+        myResultDao.newResult(result);
+
+    }
+
+    public void updateResult(Result result){
+
+        myResultDao.updateResult(result);
+
+    }
+
+    public void deleteResult(Result result){
+
+        myResultDao.deleteResult(result);
+
+    }
 }
