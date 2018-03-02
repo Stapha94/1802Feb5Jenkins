fc46b458ac4dcba0f6447f519a7cc192e7c63710
Paul Fox <pfoxiii@gmail.com>
2018-03-02 16:33:17 -0500

    Recovering some things and some more front end work

--- Paul_Fox_Code/TREE-Front-End/src/app/login-page/login-page.component.ts
+++ Paul_Fox_Code/TREE-Front-End/src/app/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { User } from '../user';
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -13,15 +15,19 @@ export class LoginPageComponent implements OnInit {
   ngOnInit() {
   }
 
-  email: string;
-  password: string;
+  user = new User("", "");
 
-  login(email, password){
+  login(){
 
-    if(email === 'test@test.test' && password === "password"){
+    if(this.user.email === 'test@test.test' && this.user.password === "password"){
       this.router.navigate(['dashboard']);
+    }
+    else{
+      alert("Incorrect Email or Password");
     }
 
   }
+
+  get diagnostic() {return JSON.stringify(this.user)}
 
 }
